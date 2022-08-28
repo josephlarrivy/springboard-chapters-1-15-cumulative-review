92e14e3b774eb76bf4215ac8d9444b0258ad5ac3
josephlarrivy <joseph.larrivy@gmail.com>
2022-08-28 17:08:14 -0500

    Still reviewing

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-//Cumulative review of all JavaScript
+//--------------------JavaScript-----------------
+
+
+
+
 
 /*
 variables and data types
@@ -25,7 +29,7 @@ variables and data types
 
 //variables
     //containers that hold data types
-    //let variables can be declared, but not assigned a value
+    //let variables can be declared, (do not need to be assigned a value when declared):
         // let num;
     //default value will be undefined
     //the variable type can change fron a number to a string to a boolean
@@ -39,7 +43,7 @@ JS fundamentals
 //strings support all unicode
 //string properties
     //strings cannot change after they are declared
-    //a string's characters can be be callex from their inxed, but not changed
+    //a string's characters can be be called from their index, but not changed
         //let name = 'Joe';
         //name[0]; returns 'J'
     //name.length; returns 3
@@ -47,7 +51,7 @@ JS fundamentals
 //string method: slice
     //strings can be sliced
         //let idk = 'I have no idea what I'm doing';
-        //idk.slick(0,13); returns 'I have no idea';
+        //idk.slice(0,13); returns 'I have no idea';
 
 //string method: includes and indexOf
     //let sentence = 'An axolotl is a tiny salamander in Mexico';
@@ -158,7 +162,7 @@ arrays
         //example:
             // [1,2,3].concat([4,5,6]);
             // returns [1,2,3,4,5,6]
-    // .join conbines array elements into a string
+    // .join conbinds array elements into a string
         //needs to be given a separator
         //example:
             // const commands = ['eat', 'drink', 'be merry'];
@@ -195,7 +199,7 @@ arrays
             //example:
                 //const biggerNums = [9, 57, 3100, 84, 101, 12];
                 //biggerNums.sort(); // [101, 12, 3100, 57, 84, 9]
-    // .slice screate copie of arrays or subsets of arays
+    // .slice creates copies of arrays or subsets of arays
         //syntax: const newArray = oldArray.slice(starting index, ending index(exclusive));
         //example:
         // const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
@@ -225,7 +229,7 @@ arrays
 objects
 */
 //objects are unordered
-//sccess object values by their key
+//access object values by their key
 //object values can be variables
 
 //accessing values in an object
@@ -245,7 +249,7 @@ objects
     //const variableName = obj.key;
     //const anotherVariableName = obj.key;
 
-//.has own property can be called on an object key to check if that key has a value
+//.hasOwn property can be called on an object key to check if that key has a value
     //object.hasOwnProperty('key');  returns true of false depending on if there is a corresponding value for the key
 
 /*
@@ -316,3 +320,241 @@ function squareValues(array){
 /*
 filter
 */
+//creates a new array
+//loops through an existing array
+//runs a callback function on existing array
+//if callback is true, pushes value to new array
+//example:
+    //function onlyInstructors(arr) {
+    //    return arr.filter(function(value) {
+    //      return arr.isInstructor;
+    //    });
+    //  }
+    //  
+    //  onlyInstructors([
+    //    { name: "Colt", isInstructor: true },
+    //    { name: "Beth", isInstructor: false },
+    //    { name: "Daniel" }
+    //  ]); // [{name: "Colt", isInstructor: true}]
+
+/*
+some/every
+*/
+//some
+    //iterate over an array
+    //run a callback on each velue
+    //if the callback returns true for any (some) of the values on the array, returns true
+    //otherwise returns false
+    //example:
+    //function hasAdmin(arr){
+    //    return arr.some(function(value){
+    //      return value.admin
+    //    });
+    //  }
+    //  
+    //  hasAdmin([
+    //    {name: "Colt", admin: true},
+    //    {name: "Poppy", admin: false}
+    //  ]); // true
+//every
+    //iterate over an array
+    //run a callback on each velue
+    //if the callback returns true for all (every) of the values on the array, returns true
+    //otherwise returns false
+    //example:
+    //function allVowels(str){
+    //    return str.split('').every(function(value){
+    //      return "aeiou".includes(value)
+    //    });
+    //  }
+    //  allVowels('awesome') // false
+    //  allVowels('aiaieoeoiu') // true
+
+    //  function allIntegers(arr){
+    //    return arr.every(Number.isInteger);
+    //  }  
+    //  allIntegers([1,2,3,4,4,4,4]) // true
+    //  allIntegers([5,1,4,3,2.2]) // false
+
+/*
+find/findIndex
+*/
+//find
+    //iterates over an array
+    //runs a callback on each value
+    //if the callback returns true for a value, return that value
+    //only returns the first value that is true, use filter to gat all values that are true
+    //example:
+    //function findOddNumber(arr){
+    //    return arr.find(function(value){
+    //      return value % 2 !== 0
+    //    });
+    //  }
+    //  
+    //  findOddNumber([2,3,4,5]) // 3
+//findIndex
+    //works the same, but returns the index of the true value, not the value itself
+    //otherwise returns -1
+
+/*
+reduce
+*/
+//accepts a callback function and an optional second parameter
+//iterates through an array
+//runs callback on each value in array
+//number returned from the call back become the new value of the accumulator
+//example:
+    //let evens = [2,4,6,8,10];
+    //evens.reduce(function(accumulator, nextValue){
+    //  return accumulator + nextValue;
+    //});
+    /*
+      2
+      6
+      12
+      20
+      30
+    */
+
+
+
+
+
+//--------------------The DOM------------------------
+
+
+
+
+
+
+//selecting elements in the DOM
+    //getElementById()
+        //document.getElementById('main');
+        //gives a single HTMLelement back
+    //getElementByTagName();
+        //returns a list of all of the element that match the requested tag
+        //looks,but does not behave like an array
+    //getElementByClassName();
+        //returns all elements with that class
+    //querySelector();
+        //returns the first element with the CSS class passed in
+    //querySelectorAll();
+        //returns all element of the CSS class passed in
+
+    //these are often used in conjunction with assigning the value of the entire selector action to a variable of a related name to be manipulated in JavaScript
+
+/*
+Modifying The DOM
+*/
+// .innerText
+    //can be used to access the inner text of an html element
+    //can also be used to change the inrrer text of an element once selected
+    //example:
+            //<section id="main-greeting">
+            //    <article>Hello World!</article>
+            //</section>
+
+        //const mainGreeting = document.getElementById("main-greeting")
+        //console.log(mainGreeting.innerText) // "Hello World!"
+        //mainGreeting.innerText = "It's changed!"
+// .textContent is similar to .innerText, but .textContent will also return any <script> or <style> tags that are in the selected html element
+// .innerHTML can be used to change the html of a element that is selected
+    //example:
+        //<section id="main-greeting">
+        //    <article>Hello World!</article>
+        //</section>
+    
+        //const mainGreeting = document.getElementById("main-greeting")
+        //mainGreeting.innerHTML = "<p>Just Changed!</p>"
+
+//modifying styling
+//select an element then assign it the value of a CSS property
+    //const mainHeading = document.querySelector("h1")
+    //mainHeading.style.color // returns the color
+    //mainHeading.style.color = "red"  //changes the color
+//the above can also be doe with background color: style.backgroundColor
+
+//modifying attributes
+    //all of the following attributes can also be changed in the DOM:
+    //src, href, class, id, type, value
+    //these attributes can be changed: example:
+        //const firstInput = document.querySelector("input")
+        //firstInput.setAttribute("type", "email")
+            //attribute is the first parameter, the value is the second
+    //some attributes (like class) can be modified directly: firstInput.id =
+
+//manipulating classes
+    //overwriting previous classes with setAttribute('class'):
+        //const mainHeading = document.querySelector("h1");
+        //mainHeading.setAttribute("class", "section-heading");
+    //finding, adding, removing classes with .classlist:
+        //const mainHeading = document.querySelector("h1")
+        //mainHeading.classList // []
+        //mainHeading.classList.add("top-heading") // ["top-heading"]
+        //mainHeading.classList.remove("top-heading") // []
+        //mainHeading.classList.toggle("top-heading") // true
+        //mainHeading.classList.contains("top-heading") // true
+
+//creating elements
+    //const newDiv = document.createElement("div");
+    //newDiv.innerText = "a brand new div!"
+    //newDiv.style.color = "tomato"
+
+//appending elements
+    //call append on a parent element to pass children into it
+    //example:
+        //const ul = document.querySelector("ul");
+        //const newLi = document.createElement("li");
+        //newLi.innerText = "Hello!";
+        //ul.append(newLi);
+
+//removing elements
+    //const ul = document.querySelector("ul");
+    //ul.remove();
+
+//elements can be accessed using their parent or child status
+    //const foundDiv = document.querySelector("div")
+    //foundDiv.parentElement // <section></section>
+    
+    //foundDiv.children // HTMLCollection(2) [p, ul]
+    //foundDiv.firstElementChild // <p>Here is a paragraph inside a div!</p>
+    //foundDiv.lastElementChild // <ul></ul>
+
+/*
+JavaScript events
+*/
+//three ways to use elements:
+    //attach function to the element in html
+    // in the HTML: <h1 onclick="runClickHandler()"> Hello World </h1>
+        //function runClickHandler(){
+          //console.log("You just clicked the h1 element!";
+        //})
+
+    //attach a function to a JavaScript element
+        //const h1 = document.querySelector("h1");
+        //h1.onclick = function(){
+            //console.log("You just clicked the h1 element!";
+        //}
+
+    //use an eventListener
+        //const h1 = document.querySelector("h1");
+        //h1.addEventListener("click", function(){
+            //console.log("You just clicked the h1 element!";
+        //})
+
+//accessing the event object
+
+
+// Need to finish events notes
+
+
+
+
+
+
+
+
+
+
+
+// ----------------------ES2015----------------------------
